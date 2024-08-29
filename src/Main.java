8fca9eef2442739843f26fc716bb864ee3f34b8e
Dmitry Parfenov <dmitry.20111@yandex.ru>
2024-08-29 09:27:28 +0300

    Added math operations with two numbers from console.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("====================");
+        System.out.printf("Сумма: %d \n", firstNumber+secondNumber);
+        System.out.printf("Разность: %d \n", firstNumber-secondNumber);
+        System.out.printf("Произведение: %d \n", firstNumber*secondNumber);
+        System.out.printf("Частное: %.3f \n", (double)firstNumber/secondNumber);
+        System.out.println("====================");
     }
 }
