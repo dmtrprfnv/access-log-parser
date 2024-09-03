34a83c2d1486512cfe08aaa8ebf16871db84e5b1
Dmitry Parfenov <dmitry.20111@yandex.ru>
2024-09-03 13:59:18 +0300

    Added checking and counting the number of file paths.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+
+        int rightPathCount = 0;
+        System.out.println("Введите путь к файлу: ");
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (isDirectory) {
+                System.out.println("Введен путь к папке! Попробуйте снова...");
+                continue;
+            } else if (fileExists) {
+                System.out.println("Путь указан верно!");
+                rightPathCount++;
+                System.out.println("Это файл номер: " + rightPathCount);
+            }
+            else System.out.println("Неверный путь к файлу! Попробуйте снова...");
+        }
     }
 }
